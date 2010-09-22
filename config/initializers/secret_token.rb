a42ec7f8389ab09f3ae56a4080dfb8e2c4a85bcc
Krishnaswamy <jskswamy@gmail.com>
2010-09-22 21:03:13 +0530

    Upgraded to Rails 3.0 and added setup instruction in README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karaharapriya::Application.config.secret_token = 'd24aca2673a5e95ceb638be2ebb1ae7836e91594804fab761017519b70d984163b788076bb3de93776a33779bdc1496d8554b390b7515d8cd64188ef65ecdb86'
