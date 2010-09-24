5e14cfdd705313f6a24a2efc7fe48fb949eba8f5
Krishnaswamy <jskswamy@gmail.com>
2010-09-24 10:02:46 +0530

    Seperated Talam from Editor, updated the tests and added few more focus test

--- public/javascripts/carnatic_editor.js
+++ public/javascripts/carnatic_editor.js
@@ -1,60 +1,117 @@
-var CarnaticEditor = Class.create({
+var Carnatic = {
 
-  initialize: function(target, talam) {
-    this.domNode = {
-      target: $(target)
-    };
-    this.options = {
-      maxLength: 1
-    };
-    this.talam = talam;
-    this._render();
-  },
+  Editor: Class.create({
 
-  _render: function() {
-    var oneCompleteTalam = Element('div');
-    this.domNode.target.appendChild(oneCompleteTalam);
+    initialize: function(target, talam) {
+      this.domNode = $(target);
+      this.talam = talam;
+      this.talamLines = [];
+      this._createNewTalamLine();
+    },
 
-    this.talam.each(function(noOfAkshram) {
-      var akshram = new Element('span');
-      for (index = 0; index < noOfAkshram; index++) {
-        var inputForAkshram = new Element('input', {
-          type: 'text',
-          maxlength: this.options.maxLength
+    _createNewTalamLine: function() {
+      var talamLine = new Carnatic.Talam(this.talam, {
+        onLastAksrham: this._focusNextTalamLine.bindAsEventListener(this)
+      });
+      this.domNode.appendChild(talamLine.domNode);
+      this.talamLines.push(talamLine);
+      return talamLine;
+    },
+
+    _getNextTalamLine: function(current) {
+
+      if (!current)
+        return;
+
+      var nextTalamDomNode = current.domNode.next();
+      var nextTalamLine;
+
+      if (nextTalamDomNode) {
+        this.talamLines.each(function(talamLine) {
+          if (talamLine.domNode == nextTalamDomNode) {
+            nextTalamLine = talamLine;
+            throw $break;
+          }
         });
-
-        inputForAkshram.observe('keypress', this._focusNext.bindAsEventListener(this));
-        akshram.appendChild(inputForAkshram);
       }
-      oneCompleteTalam.appendChild(akshram);
-    }.bind(this));
-  },
 
-  _focusNext: function(e) {
-    if (e.charCode !=0 ) {
-      var currentAkshram = e.element();
-      var nextAkshram = this._getNextAkshram(currentAkshram);
+      return nextTalamLine;
+    },
 
-      if(nextAkshram) {
-        nextAkshram.focus();
+    _focusNextTalamLine: function(current) {
+      nextTalamLine = this._getNextTalamLine(current);
+      if (!nextTalamLine)
+        nextTalamLine = this._createNewTalamLine();
+      nextTalamLine.focus();
+    },
+
+  }),
+
+  Talam: Class.create({
+
+    initialize: function(talam, options) {
+      this.talam = talam;
+      this.domNode = new Element('div');
+      this.akshramLength = 1;
+      this.options = {
+        onLastAksrham: Prototype.emptyFunction
+      };
+      Object.extend(this.options, options);
+      this._render();
+    },
+
+    _render: function() {
+      this.talam.each(function(noOfAkshram) {
+        var akshram = new Element('span');
+        for (index = 0; index < noOfAkshram; index++) {
+          var inputForAkshram = new Element('input', {
+            type: 'text',
+            maxlength: this.akshramLength
+          });
+
+          inputForAkshram.observe('keypress', this._focusNext.bindAsEventListener(this));
+          akshram.appendChild(inputForAkshram);
+        }
+        this.domNode.appendChild(akshram);
+      }.bind(this));
+    },
+
+    _getNextLaguOrDrutham: function(akshram) {
+      return akshram.up('span').next('span');
+    },
+
+    _getNextAkshram: function(akshram) {
+      var nextAkshram = akshram.next('input[type="text"]');
+
+      if(!nextAkshram) {
+        var nextLaguOrDrutham = this._getNextLaguOrDrutham(akshram)
+        if (nextLaguOrDrutham) {
+          nextAkshram = nextLaguOrDrutham.down('input[type="text"]:first-child');
+        }
       }
+
+      if (nextAkshram) {
+        return nextAkshram;
+      }
+      else {
+        this.options.onLastAksrham.apply(this, [this]);
+      }
+    },
+
+    _focusNext: function(e) {
+      if (e.charCode !=0 ) {
+        var currentAkshram = e.element();
+        var nextAkshram = this._getNextAkshram(currentAkshram);
+
+        if(nextAkshram) {
+          nextAkshram.focus();
+        }
+      }
+    },
+
+    focus: function() {
+      this.domNode.down('span > input[type="text]:first-child').focus();
     }
-  },
 
-  _getNextLaguOrDrutham: function(akshram) {
-    return akshram.up('span').next('span');
-  },
-
-  _getNextAkshram: function(akshram) {
-    var nextAkshram = akshram.next('input[type="text"]');
-
-    if(!nextAkshram) {
-      var nextLaguOrDrutham = this._getNextLaguOrDrutham(akshram)
-      if (nextLaguOrDrutham)
-        nextAkshram = nextLaguOrDrutham.select('input[type="text"]:first-child');
-    }
-
-    return nextAkshram;
-  },
-
-});
+  })
+}
--- public/javascripts/test/spec/carnatic_spec.js
+++ public/javascripts/test/spec/carnatic_spec.js
@@ -1,53 +1,59 @@
 Screw.Unit(function() {
-  describe('Carnatic Editor', function() {
-    var talam = [4,3,2];
-    var editor;
+  var SampleTalams = {
+    SangeernaJathiBhuvana: [9,2,9,9],
+    Adi: [4,2,2],
+    Rupagam: [2,4],
+    TisraJathiSutha: [3]
+  }
+  var talam;
+  var editor;
+  var editorDom = $('editor');
 
-    before(function() {
-      $('editor').update();
-      editor = new CarnaticEditor('editor', talam);
-    });
+  before(function() {
+    editorDom.update();
+    talam = SampleTalams.Adi;
+    editor = new Carnatic.Editor('editor', talam);
+  });
 
-    describe('Layout Test', function() {
+  describe('Carnatic', function() {
 
-      it('should display the specified aksharams as textboxes', function() {
-        var akshrams = $('editor').select('input[type="text"]');
-        var sum = 0;
-        talam.each(function(value) {
-          sum += value;
+    describe('Talam', function() {
+
+      describe('Layout', function() {
+        it('should display the specified aksharams as textboxes', function() {
+          var akshrams = editorDom.select('input[type="text"]');
+          var sum = 0;
+          talam.each(function(value) {
+            sum += value;
+          });
+          expect(talam).to(have_sum, sum);
         });
-        expect(talam).to(have_sum, sum);
-      });
 
-      it('aksharam textbox max lenght should be 1 by default', function() {
-        var akshrams = $('editor').select('input[type="text"]');
-        akshrams.each(function(akshram) {
-          expect(akshram).to(have_max_length, 1);
+        it('aksharam textbox max lenght should be 1 by default', function() {
+          var akshrams = editorDom.select('input[type="text"]');
+          akshrams.each(function(akshram) {
+            expect(akshram).to(have_max_length, 1);
+          });
+        });
+
+        it('should correctly group the akshram based on lagu and drutham', function() {
+          var oneCompleteTalam = editorDom.select('div:first-child');
+          var laguAndDrutham = editorDom.select('div:first-child > span');
+          expect(talam.length).to(equal, laguAndDrutham.length);
+          for(var index = 0; index < laguAndDrutham.length; index++) {
+            var akshrams = laguAndDrutham[index].select('input[type="text"]');
+            expect(talam[index]).to(equal, akshrams.length);
+          }
         });
       });
 
-      it('should correctly group the akshram based on lagu and drutham', function() {
-        var oneCompleteTalam = $('editor').select('div:first-child');
-        var laguAndDrutham = $('editor').select('div:first-child > span');
-        expect(talam.length).to(equal, laguAndDrutham.length);
-        for(var index = 0; index < laguAndDrutham.length; index++) {
-          var akshrams = laguAndDrutham[index].select('input[type="text"]');
-          expect(talam[index]).to(equal, akshrams.length);
-        }
-      });
-
-    });
-
-    describe('Focus Test', function() {
-
-      describe('First Lagu Test', function() {
-
+      describe('Focus', function() {
         var akshrams;
         var firstAkshram;
         var secondAkshram;
 
         before(function() {
-          akshrams = $('editor').select('div:nth-child(1) > span:first-child > input[type="text"]');
+          akshrams = editorDom.select('div:first-child > span:first-child > input[type="text"]');
           firstAkshram = akshrams[0];
           secondAkshram = akshrams[1];
         });
@@ -67,16 +73,53 @@ Screw.Unit(function() {
 
       });
 
-      describe('Lagu and Drutham Test', function() {
+    });
+
+    describe('Editor', function() {
+
+      describe('Layout', function() {
+        var lastAkshramInFirstTalamLine;
+        var keyEvent;
+
+        before(function() {
+          lastAkshramInFirstTalamLine = editorDom.select('div:first-child > span:last-child > input[type="text"]:last-child')[0];
+          keyEvent = new SimulateKeyboardEvent(lastAkshramInFirstTalamLine);
+        });
+
+        it('should create new talam line when the last akshram in previous talam line is entered', function() {
+          keyEvent.perform(0, 65);
+          var talamLines = editorDom.select('div');
+          var firstAkshramInSecondTalamLine = editorDom.select('div:nth-child(2) > span:first-child > input[type="text"]:first-child')[0];
+          expect(talamLines.length).to(equal, 2);
+          expect(firstAkshramInSecondTalamLine).to_not(equal, undefined);
+        });
+
+        it('should not create new talam line if the talam line is already available when the last akshram in previous talam line is entered', function() {
+          keyEvent.perform(0, 65);
+          lastAkshramInFirstTalamLine.value = "";
+          keyEvent.perform(0, 66);
+          var talamLines = editorDom.select('div');
+          expect(talamLines.length).to(equal, 2);
+        });
+
+      });
+
+      describe('Focus', function() {
+
+        it('show focus the first akshram when the editor is loaded', function() {
+          var firstAkshram = editorDom.select('div:firt-child > span:first-child > input[type="text"]:first-child')[0];
+          expect(firstAkshram).to(equal, document.activeElement);
+        });
 
         it('should focus akshram in next lagu or drutham when the last akshram in lagu or drutham is enterted', function() {
-          var lastAkshramInFirstDrutham = $('editor').select('div:nth-child(1) > span:first-child > input[type="text"]:last-child')[0];
-          var firstAkshramInFirstLagu = $('editor').select('div:nth-child(1) > span:nth-child(2) > input[type="text"]:last-child')[0];
+          var lastAkshramInFirstDrutham = editorDom.select('div:firt-child > span:first-child > input[type="text"]:last-child')[0];
+          var firstAkshramInFirstLagu = editorDom.select('div:first-child > span:nth-child(2) > input[type="text"]:first-child')[0];
+
           var keyEvent = new SimulateKeyboardEvent(lastAkshramInFirstDrutham);
           keyEvent.perform(0, 65);
-          expect("pending").to(equal, "pendiiing");
-          //expect(firstAkshramInFirstLagu).to(equal, document.activeElement);
+          expect(firstAkshramInFirstLagu).to(equal, document.activeElement);
         });
+
       });
 
     });
