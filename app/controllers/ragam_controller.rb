a06a9e055614fb4715e97eb3a42d214fa36c4f37
Krishnaswamy <jskswamy@gmail.com>
2010-10-12 23:16:35 +0530

    Added create method to ragam controller and added min len validation to ragam model

--- app/controllers/ragam_controller.rb
+++ app/controllers/ragam_controller.rb
@@ -1,7 +1,7 @@
 class RagamController < ApplicationController
 
   def create
-    Ragam.create!(:name => params[:name])
+    Ragam.create!(:name => params[:name], :arohana => params[:arohana], :avarohana => params[:avarohana])
   end
 
 end
--- spec/controllers/ragam_controller_spec.rb
+++ spec/controllers/ragam_controller_spec.rb
@@ -4,7 +4,7 @@ describe RagamController do
 
   it "should create a ragam" do
     ragam_name = "Karaharapriya"
-    post :create, :name => ragam_name
+    post :create, :name => ragam_name, :arohana => "sa re2 ga1 ma1 pa th2 ni1 sa", :avarohana => "sa ni1 th2 pa ma1 ga1 re2 sa"
     Ragam.find_by_name(ragam_name).should_not be nil
   end
 
