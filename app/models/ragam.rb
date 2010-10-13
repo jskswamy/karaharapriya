e1c50afbdfcafb29e5ece7b06f060f426c7509ec
Krishnaswamy/Usha <jskswamy@gmail.com>
2010-10-13 12:21:56 +0530

    Refactored minimum_length method in ragam model and specifed predefined width for richtext box editor

--- app/models/ragam.rb
+++ app/models/ragam.rb
@@ -5,14 +5,9 @@ class Ragam < ActiveRecord::Base
   validates_uniqueness_of :arohana, :scope => :avarohana, :message => "and Avarohana already defined for another ragam"
   validate :minimum_length
 
-  #TODO: refactor this in ruby style
   def minimum_length
-    unless self.arohana.blank?
-      errors.add(:base, "Arohana should have atleast 5 swaras") unless self.arohana.split(" ").count >= 5
-    end
-    unless self.avarohana.blank?
-      errors.add(:base, "Avarohana should have atleast 5 swaras") unless self.avarohana.split(" ").count >= 5
-    end
+    errors.add(:base, "Arohana should have atleast 5 swaras") if !self.arohana.blank? && self.arohana.split(" ").count < 5
+    errors.add(:base, "Avarohana should have atleast 5 swaras") if !self.avarohana.blank? && self.avarohana.split(" ").count < 5
   end
 
 end
