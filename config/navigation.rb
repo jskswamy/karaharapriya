ef222a4498d3497985154c611aeb3abd28d67ff5
Krishnaswamy <jskswamy@gmail.com>
2011-03-02 19:45:46 +0530

    Added basic support for tamil language

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,9 +1,9 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, '', root_path
-    primary.item :song, 'Song', songs_path
-    primary.item :ragam, 'Ragam', '#'
-    primary.item :talam, 'Talam', '#'
-    primary.item :composer, 'Composer', '#'
+    primary.item :song, t(:song), songs_path
+    primary.item :ragam, t(:ragam), '#'
+    primary.item :talam, t(:talam), '#'
+    primary.item :composer, t(:composer), '#'
   end
 end
